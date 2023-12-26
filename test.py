039245aaff50ec593995c6aa4705a169c7cdfd22
Arcyak <captainbowisse@gmail.com>
2023-12-27 00:04:59 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,45 @@
+# from astropy.healpix import *
+# import mocpy
+import subprocess
+import sys
+import re
+
+
+def extract_votable(catalogue: str, out_max: int = 10000):
+    """get VOTable from a VizieR catalogue for further studies.
+    :param catalogue : VizieR catalogue name
+    :param out_max: max number of objects
+    """
+
+    result = []
+    proc = subprocess.Popen(["/bin/sh"],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            stdin=subprocess.PIPE)
+
+    url = f'https://vizier.cds.unistra.fr/viz-bin/votable?-source={catalogue}&-out.max={out_max}'
+
+    #  execute the command
+    command = f"curl {url}"
+    proc.stdin.write(bytes(command, "ascii"))  # send command in STDIN
+    proc.stdin.close()  #  close STDIN
+
+    #  read data (STDOUT)
+    for line in proc.stdout:
+        line = line.decode().lstrip()
+        record = [float(value.strip()) for value in re.split(r" +", line)]
+        result.append(record)
+    proc.stdout.close()
+
+    # read if errors
+    for line in proc.stderr:
+        sys.stderr.write(f"(error) {line.decode()}\n")
+    proc.stderr.close()
+
+    return result
+
+
+if __name__ == '__main__':
+    print(extract_votable('/II/7A/catalog/', 10))
+
+
