c697dbaace2aae24409546620c08bad697242f4b
Arcyak <captainbowisse@gmail.com>
2023-12-27 16:24:29 +0100

    first commit

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-# from astropy.healpix import *
-# import mocpy
+import astropy_healpix
+
 import subprocess
 import sys
 import re
@@ -8,28 +8,26 @@ import re
 def extract_votable(catalogue: str, out_max: int = 10000):
     """get VOTable from a VizieR catalogue for further studies.
     :param catalogue : VizieR catalogue name
-    :param out_max: max number of objects
+    :param out_max: max number of objects, default: 10000
     """
 
     result = []
-    proc = subprocess.Popen(["/bin/sh"],
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE,
-                            stdin=subprocess.PIPE)
+    proc = subprocess.Popen(["/bin/sh"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
 
     url = f'https://vizier.cds.unistra.fr/viz-bin/votable?-source={catalogue}&-out.max={out_max}'
 
-    #  execute the command
+    # execute the command
     command = f"curl {url}"
     proc.stdin.write(bytes(command, "ascii"))  # send command in STDIN
-    proc.stdin.close()  #  close STDIN
+    proc.stdin.close()  # close STDIN
 
-    #  read data (STDOUT)
+    # read data (STDOUT)
     for line in proc.stdout:
         line = line.decode().lstrip()
-        record = [float(value.strip()) for value in re.split(r" +", line)]
+        record = [value.strip() for value in re.split(r" +", line)]
         result.append(record)
     proc.stdout.close()
+    print(result)
 
     # read if errors
     for line in proc.stderr:
@@ -40,6 +38,5 @@ def extract_votable(catalogue: str, out_max: int = 10000):
 
 
 if __name__ == '__main__':
-    print(extract_votable('/II/7A/catalog/', 10))
-
+    print(extract_votable('II/7A/catalog'))
 
